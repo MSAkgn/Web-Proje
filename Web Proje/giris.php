2008be181c857c2516d4b03280a2dc96ce1d6391
MSAkgn <muhammet.akgun2@ogr.sakarya.edu.tr>
2024-05-16 23:50:11 +0300

    Projedeki son eksikleri tamamlamaya çalışıyoruz
    İletişim sayfası tamamlandı.İletişim olarak gönderilen mesajı görmek için ayrı bir sayfaya yönlendiriliyor.

--- Web Proje/giris.php
+++ Web Proje/giris.php
@@ -1,30 +1,24 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Kullanıcı adı ve şifre POST parametrelerinden alınır
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-$username = $_POST['username'];
-$password = $_POST['password'];
+    // Geçerli e-posta adresi mi kontrol edilir
+    if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+        echo "Geçersiz kullanıcı adı";
+        exit;
+    }
 
+    // Öğrenci numarası çıkarılır
+    $studentNumber = explode('@', $username)[0];
+    $studentNumber = preg_replace('/[^0-9]/', '', $studentNumber);
 
-if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
-    header("Location: giris.html");
-    exit;
-}
-
-
-$studentNumber = explode('@', $username)[0];
-$studentNumber = preg_replace('/[^0-9]/', '', $studentNumber);
-
-
-if (empty($studentNumber) || empty($password)) {
-    header("Location: giris.html");
-    exit;
-}
-
-if ($studentNumber === $password) {
-    
-    echo "Hoşgeldiniz $studentNumber";
-} else {
-    
-    header("Location: giris.html");
-    exit;
+    // Kullanıcı adı ve şifre eşleşiyor mu kontrol edilir
+    if ($username === 'b231210032@sakarya.edu.tr' && $password === 'b231210032') {
+        echo "<script>alert('Hoşgeldiniz $studentNumber'); window.location.href = 'index.html';</script>";
+    } else {
+        echo "<script>alert('Giriş başarısız. Lütfen tekrar deneyin.'); window.location.href = 'giris.html';</script>";
+    }
 }
 ?>
